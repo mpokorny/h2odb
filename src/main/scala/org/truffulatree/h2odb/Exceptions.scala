3a49b401969cc428d9b10dc4f3f8223a0f4d48ee
Martin Pokorny <martin@truffulatree.org>
2013-07-15 20:21:45 -0600

    Added copyright statements to source code files

--- src/main/scala/org/truffulatree/h2odb/Exceptions.scala
+++ src/main/scala/org/truffulatree/h2odb/Exceptions.scala
@@ -1,3 +1,9 @@
+// Copyright 2013, Martin Pokorny <martin@truffulatree.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
+//
 package org.truffulatree.h2odb
 
 trait H2ODbException

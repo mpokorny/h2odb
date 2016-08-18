93c7865ecd008b07d9df72874c49d07f02fc1db9
Martin Pokorny <martin@truffulatree.org>
2016-08-17 20:15:35 -0600

    Add test for column value types

--- src/test/scala/xls/TableSpec.scala
+++ src/test/scala/xls/TableSpec.scala
@@ -6,12 +6,24 @@
 //
 package org.truffulatree.h2odb.xls
 
+import java.util.Date
+
 import cats.data.{OneAnd, StateT, Validated, ValidatedNel}
+import cats.std.list._
 import cats.std.option._
+import cats.syntax.option._
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.{Inside, Inspectors, OptionValues}
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.truffulatree.h2odb.UnitSpec
 
-class TableSpec extends UnitSpec with Inside with OptionValues {
+class TableSpec
+    extends UnitSpec
+    with Inspectors
+    with Inside
+    with OptionValues
+    with GeneratorDrivenPropertyChecks {
+
   implicit val rowSeqSource = RowSeq.source
 
   type State = Table.State[RowSeq.State]
@@ -111,6 +123,97 @@ class TableSpec extends UnitSpec with Inside with OptionValues {
     Inspectors.forAll (tableRows) { row =>
       inside(row) { case Validated.Valid(map@_) =>
         map.keySet should contain only ("first", "second", "third", "fourth")
+      }
+    }
+
+  }
+
+  it should "identify location of all cell value column-datatype errors" in {
+    import Arbitrary.arbitrary
+
+    val genString = for { s <- arbitrary[String] } yield CellString(s)
+
+    val genNumeric = for { d <- arbitrary[Double] } yield CellNumeric(d)
+
+    val genDate = for { d <- arbitrary[Date] } yield CellDate(d)
+
+    val genBoolean = for { b <- arbitrary[Boolean] } yield CellBoolean(b)
+
+    val genBlank = Gen.const(CellBlank)
+
+    val genFormula = for { f <- arbitrary[String] } yield CellFormula(f)
+
+    val genCellType =
+      Gen.oneOf(genString, genNumeric, genDate, genBoolean, genBlank, genFormula)
+
+    def col[T <: CellValue](g: Gen[T], n: Int, errorsIn100: Int): Gen[Seq[CellValue]] = {
+      val genCell =
+        Gen.frequency(
+          (errorsIn100, genCellType),
+          (100 - errorsIn100, g))
+
+      Gen.listOfN(n, genCell)
+    }
+
+    implicit val arbitraryTable =
+      Arbitrary {
+        Gen.sized { rows =>
+          Gen.nonEmptyContainerOf[IndexedSeq, IndexedSeq[CellValue]](
+            genCellType.flatMap(g => col(g, rows, 5).map(_.toIndexedSeq)))
+        }
+      }
+
+    GeneratorDrivenPropertyChecks.forAll { (cols: IndexedSeq[IndexedSeq[CellValue]]) =>
+
+      def transpose[A](ary: IndexedSeq[IndexedSeq[A]]): IndexedSeq[IndexedSeq[A]] =
+        (0 until ary(0).length) map { i =>
+          (0 until ary.length) map (j => ary(j)(i))
+        }
+
+      val typeMatches =
+        transpose(
+          cols map { col =>
+            col.foldLeft((none[CellValue], IndexedSeq.empty[(Boolean, CellValue)])) {
+              case ((None, acc@_), v@_) if !v.hasTypeOf(CellBlank) =>
+                (Some(v), acc :+ ((true, v)))
+
+              case ((None, acc@_), _) =>
+                (None, acc :+ ((true, CellBlank)))
+
+              case ((t@Some(v0@_), acc@_), v@_) =>
+                val typeMatch = v.hasTypeOf(v0) || v.hasTypeOf(CellBlank)
+                (t, acc :+ ((typeMatch, v0)))
+            }._2
+          })
+
+      val rows = transpose(cols)
+
+      val header = (0 until cols.length) map (i => CellString(i.toString))
+
+      val tableRows = toTable(header +: rows).zipWithIndex
+
+      Inspectors.forAll (tableRows) { case (row@_, i@_) =>
+        inside(row) {
+          case Validated.Valid(_) =>
+            typeMatches(i).map(_._1) should contain only (true)
+
+          case Validated.Invalid(errs@_) =>
+            val errsList = errs.unwrap
+
+            /* expected type descriptions should match expected */
+            Inspectors.forAll (errsList) { err =>
+              inside(err) { case Table.CellType(col@_, desc@_) =>
+                desc shouldBe typeMatches(i)(col)._2.typeDescription
+              }
+            }
+
+            /* column indexes should match expected */
+            val errCols =
+              errsList collect { case Table.CellType(col@_, _) => col }
+
+            (0 until cols.length) map (!errCols.contains(_)) should
+              contain theSameElementsInOrderAs (typeMatches(i).map(_._1))
+        } 
       }
     }
 
